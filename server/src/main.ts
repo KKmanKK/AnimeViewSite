1027c783048330be7ef859e912b1c6bb4342ceb8
npl <npl1u1pc@mail.ru>
2023-04-13 15:40:45 +0700

    ver03 seviceTs

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,11 @@ import { PrismaClient } from "@prisma/client";
 dotenv.config();
 const app: express.Application = express();
 const PORT = process.env.PORT || 5000;
+app.use(express());
 
 const start = (): void => {
-  const prisma = new PrismaClient();
-  prisma.user.findFirst();
+  const prisma = new PrismaClient()
+  prisma.anime.count();
   app.listen(PORT, () => console.log("Сервер запущен"));
 };
 start();
--- server/src/services/animeService.ts
+++ server/src/services/animeService.ts
@@ -1,8 +1,62 @@
-class AnimeService {
-  async create() {}
-  async select() {}
-  async selectById() {}
-  async delete() {}
+import { db } from "../utils/db.server.js";
+
+interface IAnimeType {
+  id: number,
+  name: string,
+  description: string | null,
+  season: string,
+  release: Date,
+  duration: string,
+  img: string,
+  frames: string,
+  trailer: string,
+  video: string,
+  viewUserId: number | null,
+  viewedUserId: number
+}
+
+interface IUserService {
+  create(anime: Omit<IAnimeType, "id">): Promise<IAnimeType>;
+  select(): Promise<IAnimeType[]>;
+  selectById(id: number): Promise<IAnimeType | null>;
+  delete(id: number): Promise<void>;
+}
+
+class AnimeService implements IUserService {
+  async create(anime: Omit<IAnimeType, "id">): Promise<IAnimeType> {
+    let { name, description, season, release, duration, img, frames, trailer, video, viewUserId, viewedUserId } = anime;
+    return await db.anime.create({
+      data: {
+        name, description, season, release, duration, img, frames, trailer, video, viewUserId, viewedUserId
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        season: true,
+        release: true,
+        duration: true,
+        img: true,
+        frames: true,
+        trailer: true,
+        video: true,
+        viewUserId: true,
+        viewedUserId: true
+      }
+    })
+  }
+  async select(): Promise<IAnimeType[]> {
+    return await db.anime.findMany();
+  }
+  async selectById(id: number): Promise<IAnimeType | null> {
+    return await db.anime.findUnique({
+      where: {
+        id
+      }
+    })
+  };
+  async delete() { }
+
 }
 
 export const animeService = new AnimeService();
